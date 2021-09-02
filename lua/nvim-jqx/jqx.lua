888cef3f7559f3bc84edbc8d9c8299b4839a5324
jon.lopez <jonathan.lopez@gridunity.com>
2021-09-02 13:59:58 -0400

    Attempt to use vim.loop to resolve filepaths

--- lua/nvim-jqx/jqx.lua
+++ lua/nvim-jqx/jqx.lua
@@ -27,7 +27,7 @@ end
 
 local function populate_qf(ft, type, sort)
    local cmd_lines = {}
-   local cur_file = string.format([["%s"]], vim.fn.getreg("%"))
+   local cur_file = vim.loop.fs_realpath(vim.fn.getreg("%"))
    if ft == 'json' then
 	  local json_types = {'string', 'number', 'vim.fn.getreg("%")boolean', 'array', 'object', 'null'}
 	  if has_value(json_types, type) then
